0fd5328386adcd37fda670c8c1caf2180210b62b
Kevin-Leeeei <2829341675@qq.com>
2025-01-02 20:28:23 +0800

    Update commarray.F90

--- src/commarray.F90
+++ src/commarray.F90
@@ -11,7 +11,8 @@ module commarray
   !
   implicit none
   !
-  real(8),allocatable,dimension(:,:,:,:) :: x,q,qrhs,vel,spc,dtmp,dgrid,vor,dvor
+  real(8),allocatable,dimension(:,:,:,:) ::  x,q,qrhs,vel,spc,dtmp,     &
+                                            dgrid,vor,dvor
   real(8),allocatable,dimension(:,:,:) :: jacob,rho,prs,tmp,vorbis
   real(8),allocatable,dimension(:,:,:,:,:) :: dxi,dvel,dspc
   real(8),allocatable,dimension(:,:,:) :: bnorm_i0,bnorm_im,bnorm_j0,  &
